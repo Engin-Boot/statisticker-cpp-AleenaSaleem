cced602c6b0e7aaa44b978594bfa9d30a6623e8f
Aleena Saleem <42008808+AleenaSaleem@users.noreply.github.com>
2020-08-31 23:42:25 +0530

    update

--- stats-test.cpp
+++ stats-test.cpp
@@ -4,13 +4,8 @@
 #include "stats.h"
 #include <cmath>
 TEST_CASE("reports average, minimum and maximum") {
-	struct Statistics::ComputedStats<float> computedFvalues;
-	/*vector<float> float_data;
-	float_data.push_back(1.5);
-	float_data.push_back(8.9);
-	float_data.push_back(3.2);
-	float_data.push_back(4.5);*/
-    auto computed_stats = Statistics::ComputeStatistics(computedFvalues,{1.5,8.9,3.2,4.5});
+	Statistics::ComputedStats<float> computedFvalues;
+    auto computed_stats = Statistics::ComputeStatistics(computedFvalues,{1.5, 8.9, 3.2, 4.5});
     float epsilon = 0.001;
     REQUIRE(std::abs(computed_stats.average - 4.525) < epsilon);
     REQUIRE(std::abs(computed_stats.Max - 8.9) < epsilon);
@@ -18,8 +13,7 @@ TEST_CASE("reports average, minimum and maximum") {
 }
 
 TEST_CASE("average is NaN for empty array") {
-	struct Statistics::ComputedStats<float> computedFvalues;
-	//vector<float> float_data;
+	 Statistics::ComputedStats<float> computedFvalues;
     auto computed_stats = Statistics::ComputeStatistics(computedFvalues, {});
     REQUIRE(isnan(computed_stats.average) == true);
     REQUIRE(isnan(computed_stats.Max) == true);
