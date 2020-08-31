ecc8c18e158ca79fd4684e26505dcc9c30ac74cb
Aleena Saleem <42008808+AleenaSaleem@users.noreply.github.com>
2020-08-31 22:47:22 +0530

    update header file

--- stats.h
+++ stats.h
@@ -1,5 +1,23 @@
+#include<iostream>
 #include <vector>
+#include<math.h>
+
+using namespace std;
+
 
 namespace Statistics {
-    Stats ComputeStatistics(const std::vector<___>& );
+	template <typename numeric_type>
+	struct ComputedStats{
+		numeric_type average;
+		numeric_type Max;
+		numeric_type Min;
+	};	
+	template <typename numeric_type>
+		void ComputeAverage(struct ComputedStats<numeric_type> &computedValues,const vector<numeric_type>& );
+	template <typename numeric_type>
+		void ComputeMax(struct ComputedStats<numeric_type> &computedValues,const vector<numeric_type>& );
+	template <typename numeric_type>
+		void ComputeMin(struct ComputedStats<numeric_type> &computedValues,const vector<numeric_type>& );
+	template <typename numeric_type>
+	struct ComputedStats<numeric_type> ComputeStatistics(struct ComputedStats<numeric_type> &computedValues,const vector<numeric_type>& );
 }
