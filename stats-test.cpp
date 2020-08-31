4e3a09387371bfade907bb1794897f44cc87687b
Aleena Saleem <42008808+AleenaSaleem@users.noreply.github.com>
2020-08-31 22:53:10 +0530

    update

--- stats-test.cpp
+++ stats-test.cpp
@@ -1,10 +1,11 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
 #include "catch.hpp"
-#include"stats.h"
+#include "stats.h"
 #include <cmath>
-Statistics::ComputedStats<float> computedFvalues;
+
 TEST_CASE("reports average, minimum and maximum") {
+    struct Statistics::ComputedStats<float> computedFvalues;
     auto computedStats = Statistics::ComputeStatistics(computedFvalues, {1.5, 8.9, 3.2, 4.5});
     float epsilon = 0.001;
     REQUIRE(std::abs(computedStats.average - 4.525) < epsilon);
@@ -13,6 +14,7 @@ TEST_CASE("reports average, minimum and maximum") {
 }
 
 TEST_CASE("average is NaN for empty array") {
+    struct Statistics::ComputedStats<float> computedFvalues;
     auto computedStats = Statistics::ComputeStatistics(computedFvalues,{});
     REQUIRE(isnan(computedStats.average) == true);
     REQUIRE(isnan(computedStats.Max) == true);
