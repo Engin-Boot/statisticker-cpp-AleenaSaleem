9f3bf4cc4503ce4bfd157567835a33c95806a4c6
Aleena Saleem <42008808+AleenaSaleem@users.noreply.github.com>
2020-08-31 23:56:33 +0530

    update

--- stats-test.cpp
+++ stats-test.cpp
@@ -5,7 +5,7 @@
 #include <cmath>
 TEST_CASE("reports average, minimum and maximum") {
 	Statistics::ComputedStats<float> computedFvalues;
-    auto computed_stats = Statistics::ComputeStatistics(computedFvalues,{1.5, 8.9, 3.2, 4.5});
+    Statistics::ComputedStats<float> computed_stats = Statistics::ComputeStatistics(computedFvalues,{1.5, 8.9, 3.2, 4.5});
     float epsilon = 0.001;
     REQUIRE(std::abs(computed_stats.average - 4.525) < epsilon);
     REQUIRE(std::abs(computed_stats.Max - 8.9) < epsilon);
@@ -14,7 +14,7 @@ TEST_CASE("reports average, minimum and maximum") {
 
 TEST_CASE("average is NaN for empty array") {
 	 Statistics::ComputedStats<float> computedFvalues;
-    auto computed_stats = Statistics::ComputeStatistics(computedFvalues, {});
+    Statistics::ComputedStats<float> computed_stats = Statistics::ComputeStatistics(computedFvalues, {});
     REQUIRE(isnan(computed_stats.average) == true);
     REQUIRE(isnan(computed_stats.Max) == true);
     REQUIRE(isnan(computed_stats.Min) == true);
