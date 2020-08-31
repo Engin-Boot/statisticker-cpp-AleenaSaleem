b0a140ab500a887211917a468ae973a58d4029a5
Aleena Saleem <42008808+AleenaSaleem@users.noreply.github.com>
2020-09-01 00:41:19 +0530

    Update

--- stats.h
+++ stats.h
@@ -13,11 +13,11 @@ namespace Statistics {
 		numeric_type Min;
 	};	
 	template <typename numeric_type>
-		void ComputeAverage(struct ComputedStats<numeric_type>&,const vector<numeric_type>& );
+		void ComputeAverage(struct ComputedStats<numeric_type>&,const vector<numeric_type, allocator<numeric_type>>& );
 	template <typename numeric_type>
-		void ComputeMax(struct ComputedStats<numeric_type>&,const vector<numeric_type>& );
+		void ComputeMax(struct ComputedStats<numeric_type>&,const vector<numeric_type, allocator<numeric_type>>& );
 	template <typename numeric_type>
-		void ComputeMin(struct ComputedStats<numeric_type>&,const vector<numeric_type>& );
+		void ComputeMin(struct ComputedStats<numeric_type>&,const vector<numeric_type,allocator<numeric_type>>& );
 	template <typename numeric_type>
-	struct ComputedStats<numeric_type> ComputeStatistics(struct ComputedStats<numeric_type>&,const vector<numeric_type>& );
+	struct ComputedStats<numeric_type> ComputeStatistics(struct ComputedStats<numeric_type>&,const vector<numeric_type, allocator<numeric_type>>& );
 }
