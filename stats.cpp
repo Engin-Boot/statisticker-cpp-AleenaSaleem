c588721a4d87fb591d3849b803472b359c50fd57
Aleena Saleem <42008808+AleenaSaleem@users.noreply.github.com>
2020-09-01 10:33:51 +0530

    update

--- stats.cpp
+++ stats.cpp
@@ -4,24 +4,24 @@
 
 namespace Statistics{
 	template <typename numeric_type>
-	void ComputeAverage(struct ComputedStats<numeric_type> &computedValues, const vector<numeric_type, allocator<numeric_type>>& data){
+	void ComputeAverage(struct ComputedStats<numeric_type> &computedValues, vector<numeric_type>& data){
 		numeric_type sum = 0;
 		sum = accumulate( data.begin(), data.end(), sum);
 		computedValues.average = sum / data.size();
 	}
 	
 	template <typename numeric_type>
-	void ComputeMax(struct ComputedStats<numeric_type> &computedValues, const vector<numeric_type, allocator<numeric_type>>& data){
+	void ComputeMax(struct ComputedStats<numeric_type> &computedValues, vector<numeric_type>& data){
 		computedValues.Max = *max_element(data.begin(), data.end());
 	}
 	
 	template <typename numeric_type>
-	void ComputeMin(struct ComputedStats<numeric_type> &computedValues, const vector<numeric_type, allocator<numeric_type>>& data){
+	void ComputeMin(struct ComputedStats<numeric_type> &computedValues, vector<numeric_type>& data){
 		computedValues.Min = *min_element(data.begin(), data.end());
 	}
 	
 	template <typename numeric_type>
-	struct ComputedStats<numeric_type> ComputeStatistics(struct ComputedStats<numeric_type> &computedValues, const vector<numeric_type,allocator<numeric_type>>& data){
+	struct ComputedStats<numeric_type> ComputeStatistics(struct ComputedStats<numeric_type> &computedValues, vector<numeric_type>& data){
 		if(data.size() == 0)
 	    	{
 			computedValues.average = numeric_limits<numeric_type>::quiet_NaN();
